2c1c87786a311590e848c1d297c4ff358167a223
JakeJeongAiden <146908415+JakeJeongAiden@users.noreply.github.com>
2023-12-15 20:27:53 -0800

    Add basic validation

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,3 +1,36 @@
+import { useState } from 'react'
+import Warning from './Warning'
+
 export default function Textarea () {
-  return <textarea className='textarea' />
+  const [text, setText] = useState('')
+
+  const [warningText, setWarningText] = useState('')
+
+  const handleChange = event => {
+    let newText = event.target.value
+
+    // basic validation
+    if (newText.includes('<script>')) {
+      setWarningText('No script tag allowed!')
+      newText = newText.replace('<script>', '')
+    } else if (newText.includes('@')) {
+      setWarningText('No @ tag allowed!')
+      newText = newText.replace('@', '')
+    } else {
+      setWarningText('')
+    }
+    setText(newText)
+  }
+
+  return (
+    <div className='textarea'>
+      <textarea
+        value={text}
+        onChange={handleChange}
+        placeholder='Enter your text here...'
+        spellCheck='false'
+      />
+      <Warning warningText={warningText} />
+    </div>
+  )
 }
