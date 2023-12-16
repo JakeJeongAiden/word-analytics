a69b5fec5e1dfaf56b2c9d5879ebd1d17bf9eecf
JakeJeongAiden <146908415+JakeJeongAiden@users.noreply.github.com>
2023-12-15 19:10:16 -0800

    Add all basic components for skeleton

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,3 +1,8 @@
 export default function Footer () {
-  return <footer>footer</footer>
+  return (
+    <footer>
+      <small>&copy; Copyright by Jake Jeong. All Rights reserved.</small>
+      <small>Last checked limits: 17 days ago</small>
+    </footer>
+  )
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,11 @@
-import Background from './Background'
+import BackgroundImage from './BackgroundImage'
+import H1 from './H1'
 export default function Header () {
   return (
     <header>
-      <Background />
+      <BackgroundImage />
+
+      <H1 />
     </header>
   )
 }
